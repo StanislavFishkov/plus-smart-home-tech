bb8a7d89898dfb0ed05ddd2635846ba9ec55fea7
Stanislav Fishkov <fishkovs@yandex.ru>
2024-12-07 16:03:58 +0100

    Make sensor events classes

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/contoller/EventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/contoller/EventController.java
@@ -1,22 +1,23 @@
 package ru.yandex.practicum.kafka.telemetry.collector.contoller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.kafka.telemetry.collector.dto.sensor.SensorEvent;
 
+@Slf4j
 @RestController
 @RequestMapping("/events")
 public class EventController {
 
     @PostMapping("/sensors")
-    public void postSensor() {
-
+    public void collectSensorEvent(@Valid @RequestBody SensorEvent event) {
+        // ... реализация метода ...
     }
 
-    @PostMapping("/hubs")
-    public void postHub() {
-
+    @PostMapping("/sensors")
+    public void collectHubEvent(@Valid @RequestBody SensorEvent event) {
+        // ... реализация метода ...
     }
 
     @GetMapping("/sensors")
