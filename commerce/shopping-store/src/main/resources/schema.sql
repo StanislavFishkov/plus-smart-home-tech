24f99d9f0c1728ed367083e0b4c0911f6c8cd491
Stanislav Fishkov <fishkovs@yandex.ru>
2025-01-09 01:14:39 +0100

    Update docker compose: commerce-db service of postgres engine with multiple dbs for each microservice

---
+++ commerce/shopping-store/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+-- создаём таблицу scenarios
+CREATE TABLE IF NOT EXISTS scenarios (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    hub_id VARCHAR,
+    name VARCHAR,
+    UNIQUE(hub_id, name)
+);
